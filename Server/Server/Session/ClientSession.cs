7b0a9747855993a7de4b6ff3a119eda5f4f012fd
yyc2911@salin.co.kr <yyc2911@salin.co.kr>
2021-03-30 21:25:20 +0900

    패킷 정보 묶는 부분을 함수로 적용

--- Server/Server/Session/ClientSession.cs
+++ Server/Server/Session/ClientSession.cs
@@ -16,6 +16,20 @@ namespace Server
 	{
 		public int SessionId { get; set; }
 
+		public void Send(IMessage packet)
+		{
+			string msgName = packet.Descriptor.Name.Replace("_", String.Empty);
+			MsgId msgId = (MsgId)Enum.Parse(typeof(MsgId), msgName);
+			
+			ushort size = (ushort)packet.CalculateSize();
+			byte[] sendBuff = new byte[size + 4]; 
+			Array.Copy(BitConverter.GetBytes(size + 4), 0, sendBuff, 0, sizeof(ushort)); // 어느정도 크기의 데이터인지
+			Array.Copy(BitConverter.GetBytes((ushort)msgId), 0, sendBuff, 2, sizeof(ushort)); // 프로토콜의 아이디
+			Array.Copy(packet.ToByteArray(), 0, sendBuff, 4, size); // 전달하려는 데이터
+			
+			Send(new ArraySegment<byte>(sendBuff));
+		}
+		
 		public override void OnConnected(EndPoint endPoint)
 		{
 			Console.WriteLine($"OnConnected : {endPoint}");
@@ -26,15 +40,7 @@ namespace Server
 				Context = "안녕하세요"
 			};
 
-			
-			ushort size = (ushort)chat.CalculateSize();
-			byte[] sendBuff = new byte[size + 4]; 
-			Array.Copy(BitConverter.GetBytes(size + 4), 0, sendBuff, 0, sizeof(ushort)); // 어느정도 크기의 데이터인지
-			ushort protocolId = 2; 
-			Array.Copy(BitConverter.GetBytes(protocolId), 0, sendBuff, 2, sizeof(ushort)); // 프로토콜의 아이디
-			Array.Copy(chat.ToByteArray(), 0, sendBuff, 4, size); // 전달하려는 데이터
-			
-			Send(new ArraySegment<byte>(sendBuff));
+			Send(chat);
 		}
 
 		public override void OnRecvPacket(ArraySegment<byte> buffer)

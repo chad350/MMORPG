15d026126d459a4a53918b7985a66566c2288acf
yyc2911@salin.co.kr <yyc2911@salin.co.kr>
2021-04-05 22:08:57 +0900

    PacketHandler 크르티컬 세션 대응 -   데이터 캐싱

--- Server/Server/Packet/PacketHandler.cs
+++ Server/Server/Packet/PacketHandler.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Text;
 using Google.Protobuf;
 using Google.Protobuf.Protocol;
+using Server.Game;
 
 class PacketHandler
 {
@@ -14,22 +15,27 @@ class PacketHandler
 		ClientSession clientSession = session as ClientSession;
 
 		Console.WriteLine($"C_Move - {movePacket.PosInfo.PoxX}, {movePacket.PosInfo.PoxY}");
-		if(clientSession.MyPlayer == null)
+		
+		// 사용하기전에 한번 캐싱해서 사용
+		Player player = clientSession.MyPlayer;
+		if(player == null)
 			return;
-		if(clientSession.MyPlayer.Room == null)
+
+		GameRoom room = player.Room;
+		if(room == null)
 			return;
 		
 		// 검증 단계 - 클라는 거짓말을 하기 때문
 		
 		// 서버에서 좌표 이동
-		PlayerInfo info = clientSession.MyPlayer.info;
+		PlayerInfo info = player.info;
 		info.PosInfo = movePacket.PosInfo;
 		
 		// 다른 플레이어에게 브로드캐스트
 		S_Move resMovePacket = new S_Move();
-		resMovePacket.PlayerId = clientSession.MyPlayer.info.PlayerId;
+		resMovePacket.PlayerId = player.info.PlayerId;
 		resMovePacket.PosInfo = movePacket.PosInfo;
 		
-		clientSession.MyPlayer.Room.Broadcast(resMovePacket);
+		room.Broadcast(resMovePacket);
 	}
 }
